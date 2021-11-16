3eecfe2d3ee7394e147f03652adc1a84f5f663d1
student <student@macbookair2-m.local>
2021-11-16 17:02:48 -0500

    overhaul  frontend

---
+++ Kiwi/Kiwi/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiwi
+//
+//  Created by student on 11/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
