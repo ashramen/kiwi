4908d5f8cee120aaf190be71276324b2c7365b3d
student <ashrahman18@gmail.com>
2021-11-20 15:35:01 -0500

    add firestore read

--- Kiwi/Kiwi/Controllers/mainViewController.swift
+++ Kiwi/Kiwi/Controllers/mainViewController.swift
@@ -11,67 +11,79 @@ import Firebase
 class mainViewController: UIViewController {
     @IBOutlet weak var coinSearchText: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    
     let db = Firestore.firestore()
     let cellIdentifier = "coinIdentifier"
-    var coins: [Coin] = [
-        Coin(name: "BTC", rate: "57384.21"),
-        Coin(name: "ETH", rate: "4856.7"),
-        Coin(name: "ETC", rate: "57.8")
-    ]
+    let coinAPI = CoinAPI()
+    
+    var coins: [Coin] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tableView.delegate = self
         tableView.dataSource = self
         navigationItem.hidesBackButton = true
         loadUI()
     }
     
     func loadUI() {
-        coins = []
-        db.collection()
-    }
-    
-    let coinAPI = CoinAPI()
-    @IBAction func searchBtn(_ sender: UIButton) {
-        if let coinName = coinSearchText.text, let userEmail = Auth.auth().currentUser?.email {
-            db.collection("favCrypto").addDocument(data: [
-                "email": userEmail,
-                "coin": coinName
-            ]) { error in
-                if let e = error {
-                    print("Data was not successfully saved, error: \(e)")
-                } else {
-                    print("Successfully saved data!")
+        let email = Auth.auth().currentUser?.email
+        db.collection("favCrypto").addSnapshotListener { querySnapshot, error in
+            if let e = error {
+                print("There was an error retrieving data: \(e)")
+            } else{
+                self.coins = []
+                if let snapshotDocs = querySnapshot?.documents {
+                    for doc in snapshotDocs{
+                        let data = doc.data()
+                        if data["email"] as? String == email{
+                            let coin = data["coin"] as? String
+                            let rate = self.coinAPI.getCoinPrice(coin: coin ?? "none", currency: "USD")
+                            let newCoin = Coin(name: coin ?? "none", rate: rate)
+                            self.coins.append(newCoin)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+
+                        }
+                    }
                 }
             }
         }
-//        let coinPrice = coinAPI.getCoinPrice(coin: coinName ?? "BTC", currency: "USD")
+    }
+    
+    
+    @IBAction func searchBtn(_ sender: UIButton) {
+        if let coinName = coinSearchText.text, let userEmail = Auth.auth().currentUser?.email {
+            if coinName != ""{
+                db.collection("favCrypto").addDocument(data: [
+                    "email": userEmail,
+                    "coin": coinName
+                ]) { error in
+                    if let e = error {
+                        print("Data was not successfully saved, error: \(e)")
+                    } else {
+                        print("Successfully saved data!")
+                    }
+                }
+            }
+            
+        }
+        //        let coinPrice = coinAPI.getCoinPrice(coin: coinName ?? "BTC", currency: "USD")
         coinSearchText.text = ""
     }
     
-//    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
-//        let firebaseAuth = Auth.auth()
-//    do {
-//      try firebaseAuth.signOut()
-//        navigationController?.popToRootViewController(animated: true)
-//    } catch let signOutError as NSError {
-//      print("Error signing out: %@", signOutError)
-//    }
-//
-//    }
+    //    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+    //        let firebaseAuth = Auth.auth()
+    //    do {
+    //      try firebaseAuth.signOut()
+    //        navigationController?.popToRootViewController(animated: true)
+    //    } catch let signOutError as NSError {
+    //      print("Error signing out: %@", signOutError)
+    //    }
+    //
+    //    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
 
 extension mainViewController: UITableViewDataSource {
@@ -85,9 +97,3 @@ extension mainViewController: UITableViewDataSource {
         return cell
     }
 }
-
-//extension mainViewController: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        <#code#>
-//    }
-//}
