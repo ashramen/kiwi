0496d199e3a3d952cb3f869808c54bf8bcad3ea0
Tristin Morse <tristin.morse@duke.edu>
2021-11-22 17:14:24 -0500

    friends tab and plan in ViewController

--- Kiwi/Kiwi/Controllers/ViewController.swift
+++ Kiwi/Kiwi/Controllers/ViewController.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
+// Further stuff to implement:
+//
+// -Error in regestration when email not formatted correctly or password too short
+// -Error in login when email already registered
+// -Showing prices and other stuff in table view for popular
+// -Navigation to more info for each coin in popular and interested coins
+// -Show table view of users when named searched in friends
+// -allow adding users
+// -allow deleting coins in favorites and users in friends
+// -create navigation to show favorites when friend is clicked in table view
+// -add log out button to home or extra tab bar page
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
