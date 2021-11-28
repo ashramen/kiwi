cdac06ce41e45ff61872d083f33dd8f9917fb938
administrator <administrator@administrators-MacBook-Pro.local>
2021-11-27 19:16:34 -0500

    Working Name and Symbol on main/popular coins.

--- Kiwi/Kiwi/Controllers/mainViewController.swift
+++ Kiwi/Kiwi/Controllers/mainViewController.swift
@@ -12,7 +12,8 @@ class mainViewController: UIViewController {
     
     let coinAPI = CoinAPI()
     var allCoins: CoinAssets = []
-    var sortedCoins: CoinAssets = []
+    var popCoins: CoinAssets = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +25,31 @@ class mainViewController: UIViewController {
         // Load all the coins from the API and Define in AllCoins
         coinAPI.getCoinAssets() { (CoinAssets) in
             self.allCoins = CoinAssets
+            
+            var count: Int = 0
+            while(self.popCoins.count < 20) {
+                
+                if (self.allCoins[count].typeIsCrypto == 1 && self.allCoins[count].priceUsd != nil) {
+                    self.popCoins.append(self.allCoins[count])
+                }
+                count += 1
+            }
             print("Total Coins Loaded: ", self.allCoins.count)
-        
-        // Test to see that the coins were loaded in descending order (volume1DayUsd)
-        /*for n in 0...40 {
+            print("Total Popular Coins : ", self.popCoins.count)
+            //print(self.popCoins[0], "\n")
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+            
+        /* Test to see that the coins were loaded in descending order (volume1DayUsd)
+        for n in 0...40 {
             print(self.allCoins[n].assetID, " - volume1DayUsd: ", self.allCoins[n].volume1DayUsd)
         }*/
-            
         }
+        
+        
         
         
     }
@@ -41,12 +59,22 @@ class mainViewController: UIViewController {
 
 extension mainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return popCoins.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "coinCell", for: indexPath) as! coinTableViewCell
-        cell.coinName.text = "Coin"
+        //let price = allCoins[indexPath.row].priceUsd!
+        print("PopCoins Tableview: ", popCoins[indexPath.row].assetID)
+        print("PopCoins Tableview: ", popCoins[indexPath.row])
+
+        cell.coinName.text = popCoins[indexPath.row].name
+        cell.coinSymbol.text = popCoins[indexPath.row].assetID
+
+        //cell.coinPrice.text = String(price)
+        //cell.coinPrice.text = String(price)
+        
+        
         return cell
     }
 }
